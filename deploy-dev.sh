ef950374513e8b8cbed20bc5730b32319b92165a
Koen Boes <kboes@qualogy.com>
2020-11-09 21:57:39 +0100

    Update stack configuration and configurability

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
+. ./windhappers-passwd-dev.env
 . ./windhappers-stack-dev.env
-
-docker service rm windhappers_cms-api
-docker service rm windhappers_cms-gui
-docker service rm windhappers_proxy
 
 docker stack deploy --compose-file windhappers-stack.yml windhappers
--- deploy-prd.sh
+++ deploy-prd.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+. ./windhappers-passwd-prd.env
 . ./windhappers-stack-prd.env
 
 docker stack deploy --compose-file windhappers-stack.yml windhappers
